2bf9e0224061aa77d67fda5a957cb2fa90e59c34
akanksha <kumbhamwar2001@gmail.com>
2023-09-26 22:54:25 +0530

    updated code

--- src/components/form.js
+++ src/components/form.js
@@ -2,36 +2,109 @@ import React, { useEffect, useState } from 'react';
 import './Form.css';
 
 const Form = ({ cities }) => {
-  const [selectedCity, setSelectedCity] = useState(''); // State to track the selected city
+  const [selectedCity, setSelectedCity] = useState('');
+  const [selectedAssembly, setSelectedAssembly] = useState('');
+  const [assemblies, setAssemblies] = useState([]);
+  const [assemblyNumbers, setAssemblyNumbers] = useState([]);
+  const [selectedAssemblyNo, setSelectedAssemblyNo] = useState(''); // Added missing state variable
   const [villages, setVillages] = useState([]);
 
-  const getVillages = async (city) => {
+  const getAssemblies = async (city) => {
     try {
       let url = `http://13.48.67.44/user/city?ct=${city}&l=0`;
       const res = await fetch(url);
       const responseData = await res.json();
-      const villagesData = responseData.data;
-      console.log("villages", villagesData);
-      setVillages(villagesData);
+      const assembliesData = responseData.data;
+
+      // Filter unique assemblies and remove duplicates
+      const uniqueAssemblies = Array.from(new Set(assembliesData.map(item => item.assembly)));
+
+      setAssemblies(uniqueAssemblies);
     } catch (error) {
       console.log(error);
     }
   };
 
+  const getAssemblyNumbers = async (city, assembly) => {
+    try {
+      let url = `http://13.48.67.44/user/city?ct=${city}&l=0`;
+      const res = await fetch(url);
+      const responseData = await res.json();
+      const assembliesData = responseData.data;
+
+      // Filter unique assembly numbers for the selected assembly
+      const uniqueAssemblyNumbers = Array.from(
+        new Set(
+          assembliesData
+            .filter(item => item.assembly === assembly)
+            .map(item => item.assembly_no)
+        )
+      );
+
+      setAssemblyNumbers(uniqueAssemblyNumbers);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const getVillagesByAssembly = async (city, assemblyNo) => {
+    try {
+      let url = `http://13.48.67.44/user/city?ct=${city}&ac=${assemblyNo}&l=0`;
+      const res = await fetch(url);
+      const responseData = await res.json();
+      const villagesData = responseData.data;
+  
+      setVillages(villagesData);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  
+
   useEffect(() => {
     if (selectedCity) {
-      // Fetch villages when a city is selected
-      getVillages(selectedCity);
-
+      getAssemblies(selectedCity);
     }
-  }, [selectedCity]); // Include selectedCity as a dependency
+  }, [selectedCity]);
 
+  useEffect(() => {
+    if (selectedCity && selectedAssembly) {
+      getAssemblyNumbers(selectedCity, selectedAssembly);
+    }
+  }, [selectedCity, selectedAssembly]);
+
+  useEffect(() => {
+    if (selectedCity && selectedAssemblyNo) {
+      // Fetch villages by assembly number and city
+      getVillagesByAssembly(selectedCity, selectedAssemblyNo);
+    }
+  }, [selectedCity, selectedAssemblyNo]);
+  
   const handleCityChange = (e) => {
     const selectedValue = e.target.value;
-    setSelectedCity(selectedValue); // Update the selected city
-    console.log("Selected city:", selectedValue); // Add this line for debugging
+    setSelectedCity(selectedValue);
+    setSelectedAssembly(''); // Clear selected assembly
+    setSelectedAssemblyNo(''); // Clear selected assembly number
+    setAssemblyNumbers([]); // Clear assembly numbers
+    setVillages([]); // Clear villages
   };
-console.log("vl",villages)
+
+  const handleAssemblyChange = (e) => {
+    const selectedValue = e.target.value;
+    setSelectedAssembly(selectedValue);
+    setSelectedAssemblyNo(''); // Clear selected assembly number
+    setVillages([]); // Clear villages
+  };
+  useEffect(() => {
+    console.log("Villages:", villages); // Add this line to check if villages are being fetched.
+  }, [villages]);
+
+  const handleAssemblyNoChange = (e) => {
+    const selectedValue = e.target.value;
+    setSelectedAssemblyNo(selectedValue);
+    setVillages([]); // Clear villages
+  };
+
   return (
     <div>
       <div className="bg"></div>
@@ -51,7 +124,7 @@ console.log("vl",villages)
               className="text-box"
               id="city"
               name="city"
-              onChange={handleCityChange} // Update selected city when the selection changes
+              onChange={handleCityChange}
             >
               <option value="">City</option>
               {cities.map((data) => (
@@ -60,20 +133,43 @@ console.log("vl",villages)
                 </option>
               ))}
             </select>
+            {/* Assemblies dropdown */}
+            <select
+              className="text-box"
+              id="assembly"
+              name="assembly"
+              onChange={handleAssemblyChange}
+            >
+              <option value="">Assembly</option>
+              {assemblies.map((assembly, index) => (
+                <option key={index} value={assembly}>
+                  {assembly}
+                </option>
+              ))}
+            </select>
+            {/* Assembly Numbers dropdown */}
+            <select
+              className="text-box"
+              id="assemblyNo"
+              name="assemblyNo"
+              onChange={handleAssemblyNoChange}
+            >
+              <option value="">Assembly No</option>
+              {assemblyNumbers.map((assemblyNo, index) => (
+                <option key={index} value={assemblyNo}>
+                  {assemblyNo}
+                </option>
+              ))}
+            </select>
             {/* Villages dropdown */}
             <select className="text-box" id="village" name="village">
-              <option value="">Village</option>
-              
-              {Array.isArray(villages) ? (
-                villages.map((villageData) => (
-                  <option key={villageData.id} value={villageData.name}>
-                    {villageData.name}
-                  </option>
-                ))
-              ) : (
-                <option value="">Loading Villages...</option>
-              )}
-            </select>
+  <option value="">Village</option>
+  {villages.length > 0 && villages.map((villageData) => (
+    <option key={villageData.id} value={villageData.name}>
+      {villageData.name}
+    </option>
+  ))}
+</select>
           </div>
         </form>
       </div>
